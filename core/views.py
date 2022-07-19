814b5a114d1f3a031f6b64b4cc1b809a41de6f75
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2022-07-19 14:58:09 +0530

    Working with views

--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@
 from django.shortcuts import render
 
 # DRF Imports
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from .serializers import PostSerializer
 from .models import Post
@@ -16,20 +18,60 @@ from .models import Post
 #     }
 #     return JsonResponse(data)
 
-class TestView(APIView):
+# class TestView(APIView):
+
+#     permission_classes = (IsAuthenticated, )
+
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         # post = qs.first() 
+#         serializer = PostSerializer(qs, many=True)
+#         # serializer = PostSerializer(post)
+#         return Response(serializer.data)
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+
+# The following three classes provide the same functionality but varied code length. 
+
+class PostView(
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):    
 
     permission_classes = (IsAuthenticated, )
 
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        # post = qs.first() 
-        serializer = PostSerializer(qs, many=True)
-        # serializer = PostSerializer(post)
-        return Response(serializer.data)
+        return self.list(self, request, *args, **kwargs)
+        
+    # def perform_create(self, serializer):
+    #     # send an email
+    #     serializer.save()
 
     def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+        return self.create(request, *args, **kwargs)
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+
+    permission_classes = (IsAuthenticated, )
+    
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, *args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+
+    permission_classes = (IsAuthenticated, )
+    
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -3,13 +3,17 @@ from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
 # from core.views import test_view
-from core.views import TestView
+# from core.views import TestView
+from core.views import PostListCreateView, PostView, PostCreateView, PostListCreateView 
 
 urlpatterns = [ 
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
     # path('', test_view, name='test')
-    path('', TestView.as_view(), name='test'),
-    path('api/token/', obtain_auth_token, name='obtain-token')
+    # path('', TestView.as_view(), name='test'),
+    path('', PostView.as_view(), name='test'),
+    path('create/', PostCreateView.as_view(), name='create'),
+    path('list-create/', PostCreateView.as_view(), name='list-create'),    
+    path('api/token/', obtain_auth_token, name= 'obtain-token')
 ]
